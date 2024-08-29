b2705711da39fe123fcb9e101fa7fe1203f0c889
Bruna <brubskarina@gmail.com>
2024-08-28 23:47:26 -0300

    update schema

---
+++ db/migrate/20240827022428_add_mining_type_to_coins.rb
@@ -0,0 +1,6 @@
+class AddMiningTypeToCoins < ActiveRecord::Migration[5.2]
+  def change
+    add_reference :coins, :mining_type, foreign_key: true
+    #add uma referencia na tabela coins, esse campo mining_type que ele está add na tabela coins, ele vai ter uma referencia (chave estrangeira)
+  end
+end
